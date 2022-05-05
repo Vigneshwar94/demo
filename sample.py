25a2946c62dca58d28abaa502bf2ece4ce0762a2
Vigneshwar94 <vigneshwar94@gmail.com>
2022-05-05 15:28:57 +0530

    Hi

--- sample.py
+++ sample.py
@@ -1,6 +1,24 @@
-def sam(): 
-  a=3
-  b=4`
-  print(a+b)
+from multipledispatch import dispatch
 
-sam()
+#passing one parameter
+@dispatch(int,int)
+def product(first,second):
+	result = first*second
+	print(result);
+
+#passing two parameters
+@dispatch(int,int,int)
+def product(first,second,third):
+	result = first * second * third
+	print(result);
+
+#you can also pass data type of any value as per requirement
+@dispatch(float,float,float)
+def product(first,second,third):
+	result = first * second * third
+	print(result);
+
+
+#calling product method with 2 arguments
+product(2,3,2) #this will give output of 12
+product(2.2,3.4,2.3)
